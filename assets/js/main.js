e652280be8a68c152aa3c10f04f2a9e31851cd47
webcodin <webcodin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-28 13:27:28 +0000

    stable tag 2.3.5
    git-svn-id: https://plugins.svn.wordpress.org/wcp-contact-form/tags/2.3.5@1274677 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+(function($) {  
+    $(document).ready(function() { 
+        
+        function refreshCaptcha (element) {
+            
+            var container = $(element).closest('.scfp-form-row').find('.scfp-captcha-image');
+            var self = element;
+            
+            var data = {};
+            data.action = 'recreateCaptcha';
+            data.nonce = ajax_scfp.ajax_nonce;
+            data.id = $(element).data('id');
+            data.key = $(element).data('key');
+
+            $.ajax({
+                url:ajax_scfp.ajax_url,
+                type: 'POST' ,
+                data: data,
+                dataType: 'json',
+                cache: false,
+                success: function(data) {
+                    if (data.img) {
+                        $(container).find('img').attr('src', data.img);
+                        $(self).closest('.scfp-form-row').find('.scfp-captcha-field .scfp-form-field').val('');                        
+                    }
+                },
+                error: function (request, status, error) {
+                }
+            });            
+        }
+        
+        
+        $('.scfp-captcha-refresh').each(function() {
+            $(this).on('click', function( event ) {
+                event.preventDefault();
+                refreshCaptcha(this);
+                return false;
+            });
+
+            refreshCaptcha(this);            
+        });
+        
+        setTimeout(function () { $('.scfp-form-notification').fadeOut('slow', function() {$(this).remove();});}, 7000);
+        
+        $(document).on('click', '.scfp-form-notifications-close', function(event) {
+           $(this).closest('.scfp-notifications').fadeOut('slow', function() {$(this).remove();}); 
+           return false;
+        }); 
+        
+        if ($('.scfp-notifications').length > 0) {
+            $('html, body').stop().animate({scrollTop: $('.scfp-notifications').eq(0).position().top} , 0);    
+        }
+
+    });
+    
+    $('.scfp-rcwidget-response').val('');
+    
+})(jQuery);
+
+
